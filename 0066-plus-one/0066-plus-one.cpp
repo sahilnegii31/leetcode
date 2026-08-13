40449090a265f2e6cbb81b53cd2c0bfcfb33f6a4
sahilnegii31 <210850374+sahilnegii31@users.noreply.github.com>
2026-08-13 22:14:19 +0530

    Time: 0 ms (100%), Space: 11.5 MB (62%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> ans;
-        int carr = 0;
-        for(int i = digits.size()-1 ; i >=0 ; i--){
-            if(i == digits.size()-1){
-                if(digits[i] == 9){   
-                    digits[i] = 0 ;
-                    carr = 1;
-                }
-                else { 
-                    digits[i] += 1;
-                }
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            digits[i]++;
+            if(digits[i]<10){
+                return digits;
             }
-            else if(digits[i] == 9 && carr ==1){
-                digits[i] = 0;
-                carr = 1;
-            }
-            else { 
-                digits[i]+=carr;
-                carr = 0;
-            }
+            digits[i]=0;
         }
-        if(digits[0] == 0 && carr == 1 ){
-            digits.insert(digits.begin(),1);
-        }
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };

f17038e4f87aea8a6e24d968e8cf0ae1cf3daf9a
sahilnegii31 <210850374+sahilnegii31@users.noreply.github.com>
2026-09-16 22:12:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (23.59%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length() ) return false;
+        string ans =  s+s;
+        if(ans.contains(goal)) return true;
+        return false;
+    }
+};
